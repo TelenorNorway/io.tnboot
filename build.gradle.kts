b5bd1af202f3e659c2ea52946f2a5003e73cae29
James Bradlee <james.bradlee@telenor.no>
2024-03-28 23:18:22 +0100

    chore: initialize Kotlin in allprojects

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,13 @@
 plugins {
+	alias(libs.plugins.kotlin.jvm)
+	alias(libs.plugins.kotlin.dokka)
+	alias(libs.plugins.kotlin.kapt)
+	alias(libs.plugins.kotlin.spring)
+	alias(libs.plugins.spring.boot)
+	alias(libs.plugins.spring.deps)
 	`maven-publish`
+	jacoco
+	`jacoco-report-aggregation`
 }
 
 allprojects {
@@ -17,5 +25,30 @@ allprojects {
 	// plugins. We skip applying the other plugins by returning early.
 	if (name == "telenor-boot-dependencies") return@allprojects
 
-	// future: add other plugins
+	apply(plugin = "org.jetbrains.kotlin.jvm")
+	apply(plugin = "org.jetbrains.dokka")
+	apply(plugin = "org.jetbrains.kotlin.kapt")
+	apply(plugin = "org.jetbrains.kotlin.plugin.spring")
+	apply(plugin = "org.springframework.boot")
+	apply(plugin = "io.spring.dependency-management")
+	apply(plugin = "jacoco")
+	apply(plugin = "jacoco-report-aggregation")
+
+	dependencies {
+		dokkaHtmlPlugin(libs.kotlin.dokka.versioning)
+	}
+
+	kotlin.jvmToolchain(21)
+
+	tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+		kotlinOptions {
+			freeCompilerArgs += "-Xjsr305=strict"
+			freeCompilerArgs += "-Xjvm-default=all"
+			jvmTarget = "21"
+		}
+	}
+
+	tasks.test {
+		useJUnitPlatform()
+	}
 }
